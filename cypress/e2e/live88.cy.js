f309d170e5f791b3ebf0783c66ea7275007d08b7
ThePixXel <TheP1xxe11@gmail.com>
2025-05-04 18:19:19 +0300

    Adds new test for live88

--- cypress/e2e/live88.cy.js
+++ cypress/e2e/live88.cy.js
@@ -1,6 +1,6 @@
 // Test #1
 describe('Live88 check visible loot boxes open up', () => {
-  it('Открывает страницу и проверяет текст', () => {
+  it('Opens the page and checks the operation and visibility of elements', () => {
    // cy.visit('https://demo.live88.io/operator/live88demo/live88-lobby/fun');
     cy.visit('https://static.live88.io/game/ld-client/latest/index.html?config_id=65ae3b3ff098df4e79a999e7&session_id=33770457-1e00-4d93-a1e1-5973e42b4e73&lobby_url=&deposit_url=&device_platform=desktop&engine_base_url=wss://app.live88.io&sub_partner_id=otcasino&lang=en&ts=1746364478769');
     cy.wait(10000); // ждём загрузку
@@ -14,15 +14,26 @@ describe('Live88 check visible loot boxes open up', () => {
 
 // Test #2
 describe('Live88 check visible open mini games', () => {
-  it('Открывает страницу и проверяет текст', () => {
+  it('Opens the page and checks the operation and visibility of elements', () => {
    // cy.visit('https://demo.live88.io/operator/live88demo/live88-lobby/fun');
     cy.visit('https://static.live88.io/game/ld-client/latest/index.html?config_id=65ae3b3ff098df4e79a999e7&session_id=33770457-1e00-4d93-a1e1-5973e42b4e73&lobby_url=&deposit_url=&device_platform=desktop&engine_base_url=wss://app.live88.io&sub_partner_id=otcasino&lang=en&ts=1746364478769');
     cy.wait(10000); // ждём загрузку
     cy.get('button[data-test-id="button-click-open-mini-games-modal"]').should('be.visible'); // проверка
-    cy.wait(8000);// ждём загрузку
+    cy.wait(4000);// ждём загрузку
     cy.get('button[data-test-id="button-click-open-mini-games-modal"]').click() // клик по кнопке
     cy.get('iframe').should('be.visible'); // проверка на видимость iframe
-    
+  });
+});
+    // Test #3
+describe('Live88 check visible open menu settings', () => {
+  it('Opens the page and checks the operation and visibility of elements', () => {
+   // cy.visit('https://demo.live88.io/operator/live88demo/live88-lobby/fun');
+    cy.visit('https://static.live88.io/game/ld-client/latest/index.html?config_id=65ae3b3ff098df4e79a999e7&session_id=33770457-1e00-4d93-a1e1-5973e42b4e73&lobby_url=&deposit_url=&device_platform=desktop&engine_base_url=wss://app.live88.io&sub_partner_id=otcasino&lang=en&ts=1746364478769');
+    cy.wait(10000); // ждём загрузку
+    cy.get('button[data-test-id="button-click-open-menu-modal"]').should('be.visible'); // проверка
+    cy.wait(5000);// ждём загрузку
+    cy.get('button[data-test-id="button-click-open-menu-modal"]').click() // клик по кнопке
+    cy.get('div[id="navigation"]').should('be.visible'); // проверка на видимость контейнера
     
     
   });
