22019a930ef1fe628e0224fe5834fc6cb44d7038
ThePixXel <TheP1xxe11@gmail.com>
2025-05-04 17:54:44 +0300

    initial

---
+++ cypress/e2e/live88.cy.js
@@ -0,0 +1,28 @@
+// Test #1
+describe('Live88 check visible loot boxes open up', () => {
+  it('Открывает страницу и проверяет текст', () => {
+   // cy.visit('https://demo.live88.io/operator/live88demo/live88-lobby/fun');
+    cy.visit('https://static.live88.io/game/ld-client/latest/index.html?config_id=65ae3b3ff098df4e79a999e7&session_id=33770457-1e00-4d93-a1e1-5973e42b4e73&lobby_url=&deposit_url=&device_platform=desktop&engine_base_url=wss://app.live88.io&sub_partner_id=otcasino&lang=en&ts=1746364478769');
+    cy.wait(10000); // ждём загрузку
+    cy.get('button[data-test-id="button-click-open-lootboxes"]').should('be.visible'); // проверка
+    cy.wait(3000);// ждём загрузку
+    cy.get('[data-test-id="button-click-open-lootboxes"]').click() // клик по кнопке
+    cy.get('[data-test-id="generic_scrollable_container"]').should('be.visible'); // проверка на видимость контейнера
+    
+  });
+});
+
+// Test #2
+describe('Live88 check visible open mini games', () => {
+  it('Открывает страницу и проверяет текст', () => {
+   // cy.visit('https://demo.live88.io/operator/live88demo/live88-lobby/fun');
+    cy.visit('https://static.live88.io/game/ld-client/latest/index.html?config_id=65ae3b3ff098df4e79a999e7&session_id=33770457-1e00-4d93-a1e1-5973e42b4e73&lobby_url=&deposit_url=&device_platform=desktop&engine_base_url=wss://app.live88.io&sub_partner_id=otcasino&lang=en&ts=1746364478769');
+    cy.wait(10000); // ждём загрузку
+    cy.get('button[data-test-id="button-click-open-mini-games-modal"]').should('be.visible'); // проверка
+    cy.wait(8000);// ждём загрузку
+    cy.get('button[data-test-id="button-click-open-mini-games-modal"]').click() // клик по кнопке
+    cy.get('iframe').should('be.visible'); // проверка на видимость iframe
+    
+    
+  });
+});
